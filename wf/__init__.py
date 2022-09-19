9eccad5a83f9ffc53203505be289c90407270c76
jvfe <jvfecav@gmail.com>
2022-09-19 14:59:49 -0300

    feat: Add workflow code and interface

--- wf/__init__.py
+++ wf/__init__.py
@@ -1,47 +1,91 @@
 import subprocess
+from pathlib import Path
 
-from latch import medium_task, workflow
-from latch.types import LatchFile
+from latch import medium_task, message, workflow
+from latch.types import LatchDir, LatchFile
 
 from .docs import metadata
 
 
 @medium_task
-def run_cemitool(input_file: LatchFile) -> LatchFile:
+def run_cemitool(
+    sample_name: str,
+    expression_data: LatchFile,
+    sample_annotation: LatchFile,
+    pathways: LatchFile,
+    interaction_table: LatchFile,
+) -> LatchDir:
     """
-    You can run R files as a subprocess:
+    Run the CEMiTool analysis pipeline, outputting an HTML report and result tables
+    """
 
-        subprocess.run(
-            [
-                "Rscript",
-                "path/to/your_script.R",
-                "command_line_arg_1",
-                "command_line_arg_2",
-                ...
-            ],
-            check=True,
-        )
-    """
+    output_directory = f"report_{sample_name}"
+    output_dir_path = Path(output_directory).resolve()
+
+    _cemitool_cmd = [
+        "Rscript",
+        "/root/cemitool.R",
+        sample_name,
+        expression_data.local_path,
+        sample_annotation.local_path,
+        pathways.local_path,
+        interaction_table.local_path,
+    ]
+
+    message(
+        "info",
+        {
+            "title": "Running CEMiTool",
+            "body": f"Running CEMiTool. Command string: {' '.join(_cemitool_cmd)}",
+        },
+    )
 
     subprocess.run(
-        [
-            "Rscript",
-            "/root/cemitool.R",
-        ],
+        _cemitool_cmd,
         check=True,
     )
 
+    if output_dir_path.exists() == False:
+        message(
+            "error",
+            {
+                "title": "No output generated from CEMiTool!",
+                "body": "CEMiTool ran but no output was generated. Check if input files are valid.",
+            },
+        )
+
+    return LatchDir(str(output_dir_path), f"latch:///CEMiTool/{output_directory}")
+
 
 @workflow(metadata)
-def cemitool(input_file: LatchFile) -> LatchFile:
+def cemitool(
+    sample_name: str,
+    expression_data: LatchFile,
+    sample_annotation: LatchFile,
+    pathways: LatchFile,
+    interaction_table: LatchFile,
+) -> LatchDir:
     """Co-expression Modules Identification Tool
 
     CEMiTool
     ----
 
-    The CEMiTool R package provides users with an easy-to-use method
+    The CEMiTool[^1] R package provides users with an easy-to-use method
     to automatically run gene co-expression analyses. In addition, it performs gene
     set enrichment analysis and over representation analysis for the gene modules
     returned by the analysis.
+
+    Read more about it
+    [here](http://bioconductor.org/packages/release/bioc/vignettes/CEMiTool/inst/doc/CEMiTool.html)
+
+    [^1]: Russo, P.S.T., Ferreira, G.R., Cardozo, L.E. et al. CEMiTool: a Bioconductor
+    package for performing comprehensive modular co-expression analyses.
+    BMC Bioinformatics 19, 56 (2018). https://doi.org/10.1186/s12859-018-2053-1
     """
-    return run_cemitool(input_file=input_file)
+    return run_cemitool(
+        sample_name=sample_name,
+        expression_data=expression_data,
+        sample_annotation=sample_annotation,
+        pathways=pathways,
+        interaction_table=interaction_table,
+    )
--- wf/docs.py
+++ wf/docs.py
@@ -12,8 +12,30 @@ metadata = LatchMetadata(
 )
 
 metadata.parameters = {
-    "input_file": LatchParameter(
-        display_name="Input File",
-        batch_table_column=True,
+    "sample_name": LatchParameter(
+        display_name="Sample name",
+        section_title="Expression Results",
+        description="Text to identify the sample by. Will define output directory.",
+    ),
+    "expression_data": LatchParameter(
+        display_name="Expression data",
+        description="CSV file with a normalized expression matrix."
+        "First column should correspond to gene identifiers while every other column should be a sample"
+        "and its respective expression values",
+    ),
+    "sample_annotation": LatchParameter(
+        display_name="Sample annotation",
+        description="CSV file specifying sample names (1st column - Named 'SampleName')"
+        "and the group they belong to (2nd column - Named 'Class'), which will be used to make contrasts",
+    ),
+    "pathways": LatchParameter(
+        display_name="GMT file to perform over-representation analysis",
+        section_title="Functional annotation data",
+        description="A GMT file with pathways/terms and genes which will be used to perform ORA analysis.",
+    ),
+    "interaction_table": LatchParameter(
+        display_name="CSV file of interactions between genes",
+        description="CSV file with interaction data."
+        "In one column a gene identifier and in the other, a gene it's linked to.",
     ),
 }
